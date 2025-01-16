07cb01598886462ef802a90ec453fd5e70ade106
Phu <tui.la.phu.6549@gmail.com>
2025-01-16 21:24:57 +0700

    study how to use route, pos, get,req,....

--- server.js
+++ server.js
@@ -1,9 +1,13 @@
-const express = require("express");
-const app = express();
-const port = 8080;
-
+const express = require("express"); //commonjs
+const app = express(); // khởi tạo express
+const port = 8080; //port
+// khai báo route ("/") và trả về hành động
 app.get("/", (req, res) => {
   res.send("Hello World!");
+});
+// syntax: app.method(path, handler(là function xử lý khi route được match)) - method: get, post, put, delete ...
+app.get("/abc", (req, res) => {
+  res.send("<h1>check abc<h1>");
 });
 
 app.listen(port, () => {
