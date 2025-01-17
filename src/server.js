866d6ac68e55093f6d8ddeea0faf2ec181c3dbe6
Phu <tui.la.phu.6549@gmail.com>
2025-01-17 09:31:13 +0700

    study about engine template and how to use it

--- src/server.js
+++ src/server.js
@@ -1,6 +1,13 @@
 const express = require("express"); //commonjs
+const path = require("path"); //them thu vien path
+
 const app = express(); // khởi tạo express
 const port = 8080; //port
+
+// config template engine
+app.set("views", path.join(__dirname, "views")); // sử dụng path để join đường dẫn
+app.set("view engine", "ejs");
+
 // khai báo route ("/") và trả về hành động
 app.get("/", (req, res) => {
   res.send("Hello World!");
@@ -8,6 +15,11 @@ app.get("/", (req, res) => {
 // syntax: app.method(path, handler(là function xử lý khi route được match)) - method: get, post, put, delete ...
 app.get("/abc", (req, res) => {
   res.send("<h1>check abc<h1>");
+});
+
+app.get("/yamato", (req, res) => {
+  // res.send("<h1>check abc<h1>");
+  res.render("sample.ejs");
 });
 
 app.listen(port, () => {
